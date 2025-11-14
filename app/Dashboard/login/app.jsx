53571090ff151c5f114b5537dbd1170dab0d7837
Bradon177 <trillosbrandon262@gmail.com>
2025-11-14 07:48:24 -0500

    Login page

--- app/Dashboard/login/app.jsx
+++ app/Dashboard/login/app.jsx
@@ -9,6 +9,25 @@ export const app = () => {
 
 
   return (
-    <div>app</div>
+    <form>
+
+
+        <label>ingresa tu correo</label>
+        <input type='text' placeholder='Ingresa tu correo' />
+
+
+
+        <label >Contraseña</label>
+        <input type='password' placeholder='Ingresa tu contraseña'/>
+
+
+        <button type='submit'>Iniciar sesion</button>
+        <button>Iniciar con Google</button>
+
+
+
+
+
+    </form>
   )
 }
