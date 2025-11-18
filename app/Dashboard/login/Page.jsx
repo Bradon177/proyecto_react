efd3c7a5ad636abd4bbec17cb3695540c4722976
Bradon177 <trillosbrandon262@gmail.com>
2025-11-18 08:19:17 -0500

    arreglo de  redireccion de rutas

--- app/Dashboard/login/Page.jsx
+++ app/Dashboard/login/Page.jsx
@@ -8,14 +8,14 @@ export default function Page() {
 
   const [show, setShow] = useState(false);
 
-  const handleClick = () =>{
+  const handleClick = () => {
 
     router.push("/register")
 
   }
-  const handleRecuperar = () =>{
+  const handleRecuperar = () => {
 
-    router.push("/recuperar") 
+    router.push("/recuperar")
   }
 
   return (
@@ -119,7 +119,11 @@ export default function Page() {
         {/* Recuperar contraseña */}
         <p className="text-center text-gray-600 text-sm">
           ¿Olvidaste tu contraseña?
-          <button onClick={handleRecuperar} className="text-blue-600 hover:text-blue-700 font-medium ml-1">
+          <button
+            type="button"
+            onClick={handleRecuperar}
+            className="text-blue-600 hover:text-blue-700 font-medium ml-1"
+          >
             Recuperarla
           </button>
         </p>
